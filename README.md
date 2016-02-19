4ccd9c1efb748e70c75692535bc8e0d50f94649c
dragon-ryderz <josh@ipartnr.com>
2016-02-19 15:30:55 -0700

    v2

--- README.md
+++ README.md
@@ -1,24 +1,44 @@
 # LiteBox
-#### v0.1.1
+#### v0.2.0
+LiteBox is a lightweight, jQuery-dependent lightbox library
 LiteBox is designed to be super easy to use.  It integrates with jQuery and automates everything for you.
 
-## Perks:
+## Coolness Factors:
 
 - LiteBox adds the `.LiteBox()` method to the `jQuery.fn` object.  Usage thus becomes as simple as `$('.my-selector').LiteBox()`.
-- The JavaScript file is all that's required.  LiteBox will automatically inject a CSS blob onto your page to style all its lightboxes.
+- The JavaScript file is all that's required.  LiteBox will automatically inject a (super small) CSS blob onto your page to style all its lightboxes.
 - LiteBox is perfectly responsive.  It works on all screen sizes on all modern browsers.
+- LiteBox can be made to lazy-load the contents of its lightboxes.
 
 ## Example Usage
-### HTML:
+#### HTML:
 
 ```
 	<a class="litebox-trigger" href="/path/to/file.html">Click Me</a>
 ```
 
-### JavaScript:
+#### JavaScript:
 
 ```
 	$('.litebox-trigger').LiteBox();
 ```
 
 LiteBox will automatically pull the contents for each lightbox from the file specified by the `href` attribute on the `a` tag.
+
+## Customization
+#### In the HTML:
+
+There are 3 `data-` attributes that you can add to the trigger element:
+
+- `data-size`.  Possible values: `sm | md | lg`.  Apply this to make the default size of the generated lightbox (on bigger screen sizes)...well...small, medium, or large.
+- `data-class`.  This is for when you really need some customization.  The front of the generated lightbox will receive the value of this property as a CSS class that you can target and style to your heart's content.
+> Inside Tip
+> All LiteBox styles use single-level CSS class selectors, making it super easy to out-qualify them with your own CSS.  LiteBox also puts its styles at the very beginning of your page's `<head>` tag, ensuring that all your styles come after them and, therefore, override them.
+- `data-template`.  Example: `data-template="#tpl-id"` where `#tpl-id` is the CSS selector of an element on your page (such as a `<script type="text/template" id="tpl-id">...</script` tag) wherein are found the contents of the lightbox.  This replaces the `href` attribute on the trigger element.  When this is specified, LiteBox will pull the contents from `$('#tpl-id').html()`, instead of HTTP-requesting the page.
+
+#### In the CSS:
+
+LiteBox allows you to easily override its default styles.  Additionally, a `data-class` attribute on the trigger element allows per-lightbox customization.  Here are some notes on styling the lightboxes:
+
+- To change the default size (on larger screens) of a lightbox, modify the `height` or `width` CSS properties.
+- To change the responsiveness of the lightbox, modify the `max-height` or `max-width` properties.  These properties should remain percentages, but you can do what you want ;)
--- src/jquery.litebox.js
+++ src/jquery.litebox.js
@@ -1,5 +1,5 @@
 /**
- * LiteBox -- version 0.1.1 -- Jan. 22, 2016
+ * LiteBox -- version 0.2.0 -- Jan. 22, 2016
  * 
  * Another library by Joshua Claunch
  *   https://github.com/bowheart
@@ -15,9 +15,13 @@
 			return;
 		}
 		
-		this.triggerEl = triggerEl
+		this.triggerEl = triggerEl = $(triggerEl);
+		this.size = triggerEl.data('size') || 'md';
+		this.class = (triggerEl.data('class') ? ' ' + triggerEl.data('class') : '');
+		this.template = triggerEl.data('template');
+		
 		this.el = $(this.html); // generate the wrapping HTML
-		this.loadContents(triggerEl.href); // load the content HTML
+		this.loadContents(triggerEl.attr('href')); // load the content HTML
 		
 		// bind events
 		this.bindOpen();
@@ -27,9 +31,15 @@
 	
 		bindClose: function() {
 			var self = this;
-			self.el.find('.x').andSelf().click(function(event) {
+			self.el.one('click', function(event) {
 				event.preventDefault();
 				self.hide();
+			}).one('click', '.litebox-x, .close', function(event) {
+				event.preventDefault();
+				self.hide();
+			});
+			self.el.find('.litebox-front').off('click').click(function(event) {
+				event.stopPropagation();
 			});
 		},
 		bindOpen: function() {
@@ -39,17 +49,22 @@
 				$('body').append(self.el);
 				self.show();
 			});
-			self.el.find('.litebox-front').click(function(event) {
-				event.stopPropagation();
-			});
 		},
 		
 		hide: function() {
-			this.el.fadeOut(300, this.el.remove);
+			var self = this;
+			this.el.fadeOut(300, function() {
+				$(this).remove();
+				self.bindClose();
+			});
 		},
 		
 		loadContents: function(href) {
-			this.el.find('.litebox-content').load(href);
+			var content = this.el.find('.litebox-content');
+			
+			this.template
+				? content.html($(this.template).html())
+				: content.load(href);
 		},
 		
 		show: function() {
@@ -58,19 +73,22 @@
 		
 		
 		get html() {
-			return '<div class="litebox"><div class="litebox-front"><span class="x">X</span><div class="litebox-content"><p>Error: The content could not be loaded.</p></div></div></div>';
+			return '<div class="litebox"><div class="litebox-front litebox-' + this.size + this.class + '"><span class="litebox-x">&times;</span><div class="litebox-content"><p>Error: The content could not be loaded.</p></div></div></div>';
 		}
 	};
 	
 	
 	var css = '.litebox { overflow: hidden; position: fixed; top: 0; left: 0; text-align: center; height: 100%; width: 100%; z-index: 15; }'
 			+ '.litebox:before { background: rgba(0,0,0,0.4); content: ""; cursor: pointer; position: absolute; top: -100%; left: -100%; height: 300%; width: 300%; }'
-			+ '.litebox-front { background: #fff; box-shadow: 2px 2px 10px; box-sizing: border-box; display: inline-block; max-height: 94%; max-width: 94%; overflow: auto; padding: 20px; position: relative; text-align: left; vertical-align: middle; height: 800px; width: 960px; }'
-			+ '.litebox-front .x { color: rgba(0,0,0,0.7); cursor: pointer; font-family: arial; font-size: 32px; font-weight: bold; position: absolute; top: 10px; right: 20px; }'
-			+ '.litebox-front .x:hover { color: rgba(0,0,0,0.8); }'
+			+ '.litebox-front { background: #fff; box-shadow: 2px 2px 10px #000; box-sizing: border-box; display: inline-block; max-height: 94%; max-width: 94%; overflow: auto; padding: 20px; position: relative; text-align: left; vertical-align: middle; }'
+			+ '.litebox-sm { height: 500px; width: 375px; }'
+			+ '.litebox-md { height: 585px; width: 780px; }'
+			+ '.litebox-lg { height: 750px; width: 1000px; }'
+			+ '.litebox-x { color: rgba(0,0,0,0.7); cursor: pointer; font-family: arial; font-size: 32px; font-weight: bold; position: absolute; top: 10px; right: 20px; text-shadow: 0 0 rgba(255,255,255,0.5); }'
+			+ '.litebox-x:hover { color: rgba(0,0,0,1); }'
 			+ '.litebox:after { content: ""; display: inline-block; height: 100%; position: relative; vertical-align: middle; }'
 	var blob = new Blob([css], {type: 'text/css'});
-	$('head').append('<link rel="stylesheet" href="' + window.URL.createObjectURL(blob) + '">');
+	$('head').prepend('<link rel="stylesheet" href="' + window.URL.createObjectURL(blob) + '">');
 	
 	// Expose this function to jQuery
 	$.fn.LiteBox = function() {
