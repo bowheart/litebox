75b46d05bde2ead2cd30cf23525613b8c6e12ac3
dragon-ryderz <josh@ipartnr.com>
2016-03-04 15:26:10 -0700

    Fix "x" to top of litebox, add text faders, add new sizes

--- src/jquery.litebox.js
+++ src/jquery.litebox.js
@@ -1,13 +1,13 @@
 /**
  * LiteBox -- version 0.2.0 -- Jan. 22, 2016
- * 
+ *
  * Another library by Joshua Claunch
  *   https://github.com/bowheart
  *   https://gist.github.com/bowheart
  *
  * A jQuery-dependent lightbox library
  */
- (function($) {
+(function($) {
 	
 	var LiteBox = function(triggerEl) {
 		if (!triggerEl) {
@@ -80,12 +80,18 @@
 	
 	var css = '.litebox { overflow: hidden; position: fixed; top: 0; left: 0; text-align: center; height: 100%; width: 100%; z-index: 15; }'
 			+ '.litebox:before { background: rgba(0,0,0,0.4); content: ""; cursor: pointer; position: absolute; top: -100%; left: -100%; height: 300%; width: 300%; }'
-			+ '.litebox-front { background: #fff; box-shadow: 2px 2px 10px #000; box-sizing: border-box; display: inline-block; max-height: 94%; max-width: 94%; overflow: auto; padding: 20px; position: relative; text-align: left; vertical-align: middle; }'
-			+ '.litebox-sm { height: 500px; width: 375px; }'
-			+ '.litebox-md { height: 585px; width: 780px; }'
-			+ '.litebox-lg { height: 750px; width: 1000px; }'
-			+ '.litebox-x { color: rgba(0,0,0,0.7); cursor: pointer; font-family: arial; font-size: 32px; font-weight: bold; position: absolute; top: 10px; right: 20px; text-shadow: 0 0 rgba(255,255,255,0.5); }'
+			+ '.litebox-front { background: #fff; box-shadow: 2px 2px 10px #000; box-sizing: border-box; display: inline-block; max-height: 94%; max-width: 94%; overflow: hidden; position: relative; text-align: left; vertical-align: middle; }'
+			+ '.litebox-xs { height: 624px; width: 351px; }'
+			+ '.litebox-sm { height: 441px; width: 784px; }'
+			+ '.litebox-md { height: 531px; width: 944px; }'
+			+ '.litebox-lg { height: 621px; width: 1104px; }'
+			+ '.litebox-xl { height: 711px; width: 1264px; }'
+            + '.litebox-front:before, .litebox-front:after { content: ""; height: 40px; position: absolute; left: 0; right: 16px; }'
+            + '.litebox-front:before { background: linear-gradient(#fff 15%, rgba(255,255,255,0.1)); top: 0; }'
+            + '.litebox-front:after { background: linear-gradient(rgba(255,255,255,0.1), #fff 85%); bottom: 0; }'
+			+ '.litebox-x { color: rgba(0,0,0,0.7); cursor: pointer; font-family: arial; font-size: 32px; font-weight: bold; position: absolute; top: -7px; right: 20px; text-shadow: 0 0 rgba(255,255,255,0.5); z-index: 1; }'
 			+ '.litebox-x:hover { color: rgba(0,0,0,1); }'
+            + '.litebox-content { box-sizing: border-box; overflow: auto; padding: 40px 20px; height: 100%; width: 100%; }'
 			+ '.litebox:after { content: ""; display: inline-block; height: 100%; position: relative; vertical-align: middle; }'
 	var blob = new Blob([css], {type: 'text/css'});
 	$('head').prepend('<link rel="stylesheet" href="' + window.URL.createObjectURL(blob) + '">');
